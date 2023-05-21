62a7d976622bd0b7118d174b158d5a44662e32db
Juliano Soares da Silva <julianosoaresdasilva@outlook.com>
2023-05-21 18:25:07 +0000

    add plug and coc

--- init.vim
+++ init.vim
@@ -1,5 +1,13 @@
+
+call plug#begin()
+
+Plug 'neoclide/coc.nvim', {'branch': 'release'}
+
+call plug#end()
+
 set tabstop=4
 set shiftwidth=4
 set expandtab
 set number
 set relativenumber
+
---
+++ install_ext.sh
@@ -0,0 +1,1 @@
+nvim -c 'CocInstall -sync coc-json coc-html coc-css coc-prettier coc-tsserver coc-prisma coc-pyright coc-rust-analyzer coc-toml coc-sh coc-clangd coc-docker coc-go coc-julia coc-lua coc-yaml|q'
